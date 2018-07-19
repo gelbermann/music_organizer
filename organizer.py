cba4281a43afa08b94fb334db9faf1fadb8bce88
gelbermann <niv.gelbermann@gmail.com>
2018-07-19 10:54:58 +0300

    Formatted output

--- organizer.py
+++ organizer.py
@@ -2,6 +2,8 @@ import os
 import id3reader_p3 as id3reader
 from shutil import move, SameFileError, rmtree
 
+log_file = None
+
 
 class FileTags:
 	"""
@@ -46,11 +48,11 @@ class FileTags:
 		self._year = year if year else "yyyy"
 
 
-def display_contents(dir_path: str):
-	for path, dirs, files in os.walk(dir_path):
-		print("Subdirectories under " + path + ": " + str(dirs))
-		print("Files: " + str(files))
-		print()
+# def display_contents(dir_path: str):
+# 	for path, dirs, files in os.walk(dir_path):
+# 		print("Subdirectories under " + path + ": " + str(dirs))
+# 		print("Files: " + str(files))
+# 		print()
 
 
 def organize(dir_path: str, name_pattern: str = "%A/%y - %a") -> None:
@@ -65,19 +67,16 @@ def organize(dir_path: str, name_pattern: str = "%A/%y - %a") -> None:
 					tag.artist = reader.get_value('performer')
 					tag.album = reader.get_value('album')
 					tag.year = reader.get_value('year')
-				except id3reader.Id3Error as e:
-					print("Id3Error!: " + str(e))
-				except Exception as e:
-					print("General Error!: " + str(e))
+				except id3reader.Id3Error as error:
+					print("[ERROR] Couldn't read tags from file: {}".format(os.path.join(path, file)), file)
+					print("\t{}".format(error))
+				except Exception as error:
+					print("[ERROR] Unknown error occurred, please investigate.")
+					print("\t{}".format(error))
 				else:  # executed if there are no exceptions
 					dir_name = generate_directory_name(tag, name_pattern)
 					dst_path = create_directory(abs_path, dir_name)
-					print()
-					print("path:\t{}".format(path))
-					# copy_file(path, file, dst_path)
 					move_file(path, file, dst_path)
-				finally:
-					print()
 
 
 def is_media_file(file: str):
@@ -103,15 +102,14 @@ def create_directory(dir_path: str, dir_name: str) -> str:
 	new_path = os.path.join(os.path.abspath(dir_path), dir_name)
 	try:
 		if not os.path.exists(new_path):
-			print('Creating directory "{}" at path {}'.format(dir_name, dir_path))
 			os.makedirs(new_path)
 		else:
-			print('Directory "{}" already exists, no action taken'.format(dir_name))
+			print('[!] Directory "{}" already exists, no action taken.'.format(dir_name))
 	except OSError as error:
-		print('Error creating directory "{}" at path {}'.format(dir_name, dir_path))
-	# print(error)
-	finally:
-		print()
+		print("[ERROR] Could not create directory: {}".format(os.path.join(dir_path, dir_name)))
+		print("\t{}".format(error))
+	else:
+		print("[!] Directory {} created successfully.".format(dir_name))
 	return new_path
 
 
@@ -132,63 +130,41 @@ def generate_directory_name(tag: FileTags, pattern: str) -> str:
 		.replace('%a', tag.album)
 
 
-# def copy_file(src_path: str, file_name: str, dst_path: str) -> None:
-# 	src_path = os.path.join(src_path, file_name)
-#
-# 	try:
-# 		shutil.copy(src_path, dst_path)
-# 	except shutil.SameFileError:
-# 		print("File '{}' is already in destination, no action taken".format(file_name))
-# 	except OSError as error:
-# 		print("Error copying file to destination '{}' "
-# 			  "destination is not writeable or inaccessible.".format(dst_path))
-# 		print(error)
-# 	else:
-# 		print("File {} copied successfully".format(file_name))
-# 	finally:
-# 		print()
-
-
 def move_file(src_path: str, file_name: str, dst_path: str) -> None:
 	src_path = os.path.join(src_path, file_name)
 
 	try:
 		move(src_path, dst_path)
 	except SameFileError:
-		print("File '{}' is already in destination, no action taken".format(file_name))
+		print("[!] File '{}' is already in destination, no action taken.".format(file_name))
 	except OSError as error:
-		print("Error moving file to destination '{}' - "
-			  "destination is inaccessible or not writeable.".format(dst_path))
-		print(error)
+		# print("Error moving file to destination '{}' - "
+		# 	  "destination is inaccessible or not writeable.".format(dst_path))
+		# print(error)
+		print("[ERROR] Could not move file to destination: {}".format(dst_path))
+		print("\t{}".format(error))
 	else:
-		print("File {} moved successfully".format(file_name))
-	finally:
-		print()
+		print("[!] File {} moved successfully.".format(file_name))
 
 
 def clear_remains(dir_path: str) -> None:
 	abs_path = os.path.abspath(dir_path)
-
-	display_contents(dir_path)
-
 	for path, dirs, files in os.walk(abs_path):
 		for directory in dirs:
 			if contains_no_media(os.path.join(path, directory)):
-				print("Directory {} contains no media".format(directory))
+				# print("[!]Directory {} contains no media".format(directory))
 				try:
-					print("trying to delete {}\\{}".format(path, directory))
+					# print("trying to delete {}\\{}".format(path, directory))
 					rmtree(os.path.join(path, directory))
 				except Exception as error:
-					print("Error deleting directory {}".format(directory))
-					print(error)
+					print("[ERROR] Could not delete directory: {}".format(directory))
+					print("\t{}".format(error))
 				else:
-					print("Directory {} deleted successfully".format(directory))
+					print("[!] Directory {} deleted successfully.".format(directory))
 
 
 def contains_no_media(dir_path: str) -> bool:
-	print("====== {} ======".format(dir_path))
 	for item in os.listdir(dir_path):
-		print(item)
 		item_path = os.path.join(dir_path, item)
 		if (os.path.isfile(item_path) and is_media_file(item_path)) \
 				or os.path.isdir(item_path):
@@ -198,8 +174,13 @@ def contains_no_media(dir_path: str) -> bool:
 
 def main():
 	root = "D:\\CodeProjects\\Python\\music_test_folder"
+
+	log_file = open(os.path.join(root, 'organizer.log'), 'w')
+
 	organize(root)
 	clear_remains(root)
+
+	log_file.close()
 
 
 if __name__ == '__main__':
