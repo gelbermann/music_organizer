d70cbee1561901151351b1341742cb563a42f7d3
gelbermann <niv.gelbermann@gmail.com>
2018-07-19 15:57:50 +0300

    Album art fetching implemented Using the Last.fm API and third-party pylast module.

--- organizer.py
+++ organizer.py
@@ -1,8 +1,8 @@
 import os
 import id3reader_p3 as id3reader
 from shutil import move, SameFileError, rmtree
-
-log_file = None
+import pylast
+import urllib.request as request
 
 
 class FileTags:
@@ -56,27 +56,60 @@ class FileTags:
 
 
 def organize(dir_path: str, name_pattern: str = "%A/%y - %a") -> None:
-	tag = FileTags(None, None, None)
+	# tag = FileTags(None, None, None)
 	abs_path = os.path.abspath(dir_path)
 
+	tag = None
 	for path, dirs, files in os.walk(abs_path):
 		for file in files:
 			if is_media_file(file):
-				try:
-					reader = id3reader.Reader(os.path.join(path, file))
-					tag.artist = reader.get_value('performer')
-					tag.album = reader.get_value('album')
-					tag.year = reader.get_value('year')
-				except id3reader.Id3Error as error:
-					print("[ERROR] Couldn't read tags from file: {}".format(os.path.join(path, file)), file)
-					print("\t{}".format(error))
-				except Exception as error:
-					print("[ERROR] Unknown error occurred, please investigate.")
-					print("\t{}".format(error))
-				else:  # executed if there are no exceptions
+				file_path = os.path.join(path, file)
+				# try:
+				# 	reader = id3reader.Reader(file_path)
+				# 	tag.artist = reader.get_value('performer')
+				# 	tag.album = reader.get_value('album')
+				# 	tag.year = reader.get_value('year')
+				# except id3reader.Id3Error as error:
+				# 	print("[ERROR] Couldn't read tags from file: '{}'".format(file_path), file)
+				# 	print("\t{}".format(error))
+				# except Exception as error:
+				# 	print("[ERROR] Unknown error occurred, please investigate.")
+				# 	print("\t{}".format(error))
+				# else:  # executed if there are no exceptions
+				# 	dir_name = generate_directory_name(tag, name_pattern)
+				# 	dst_path = create_directory(abs_path, dir_name)
+				# 	move_file(file_path, dst_path)
+				tag = generate_tag(file_path)
+				if tag:
 					dir_name = generate_directory_name(tag, name_pattern)
 					dst_path = create_directory(abs_path, dir_name)
-					move_file(path, file, dst_path)
+					move_file(file_path, dst_path)
+
+
+def generate_tag(file_path: str) -> FileTags:
+	"""
+	Generates FileTags object for given media file.
+
+	:param file_path: file's full path.
+	:return: FileTags object. If errors occur, returns None.
+	"""
+	_, file_name = os.path.split(file_path)
+	tag = FileTags(None, None, None)
+
+	try:
+		reader = id3reader.Reader(file_path)
+		tag.artist = reader.get_value('performer')
+		tag.album = reader.get_value('album')
+		tag.year = reader.get_value('year')
+	except id3reader.Id3Error as error:
+		print("[ERROR] Couldn't read tags from file: '{}'".format(file_name))
+		print("\t{}".format(error))
+		tag = None
+	except Exception as error:
+		print("[ERROR] Unknown error occurred, please investigate.")
+		print("\t{}".format(error))
+		tag = None
+	return tag
 
 
 def is_media_file(file: str):
@@ -103,13 +136,12 @@ def create_directory(dir_path: str, dir_name: str) -> str:
 	try:
 		if not os.path.exists(new_path):
 			os.makedirs(new_path)
-		else:
-			print('[!] Directory "{}" already exists, no action taken.'.format(dir_name))
+			print("[!] Directory '{}' created successfully.".format(dir_name))
+	# else:
+	# 	print('[!] Directory "{}" already exists, no action taken.'.format(dir_name))
 	except OSError as error:
-		print("[ERROR] Could not create directory: {}".format(os.path.join(dir_path, dir_name)))
+		print("[ERROR] Could not create directory: '{}'".format(os.path.join(dir_path, dir_name)))
 		print("\t{}".format(error))
-	else:
-		print("[!] Directory {} created successfully.".format(dir_name))
 	return new_path
 
 
@@ -125,18 +157,23 @@ def generate_directory_name(tag: FileTags, pattern: str) -> str:
 
 	# TODO handle all possible pattern values
 	return pattern \
-		.replace('%A', tag.artist.replace('/', '_')) \
+		.replace('%A', tag.artist.replace('/', '//')) \
 		.replace('%y', tag.year) \
 		.replace('%a', tag.album)
 
 
-def move_file(src_path: str, file_name: str, dst_path: str) -> None:
-	src_path = os.path.join(src_path, file_name)
+def move_file(file_path: str, dst_path: str) -> None:
+	"""
+	Moves file to new location.
 
+	:param file_path: full path for file, as string
+	:param dst_path: destination directory's path, as string
+	"""
 	try:
-		move(src_path, dst_path)
+		move(file_path, dst_path)
 	except SameFileError:
-		print("[!] File '{}' is already in destination, no action taken.".format(file_name))
+		# print("[!] File '{}' is already in destination, no action taken.".format(file_name))
+		pass
 	except OSError as error:
 		# print("Error moving file to destination '{}' - "
 		# 	  "destination is inaccessible or not writeable.".format(dst_path))
@@ -144,10 +181,19 @@ def move_file(src_path: str, file_name: str, dst_path: str) -> None:
 		print("[ERROR] Could not move file to destination: {}".format(dst_path))
 		print("\t{}".format(error))
 	else:
-		print("[!] File {} moved successfully.".format(file_name))
+		_, tail = os.path.split(file_path)
+		print("[!] File '{}' moved successfully.".format(tail))
 
 
 def clear_remains(dir_path: str) -> None:
+	"""
+	Deletes every directory that is either empty,
+	or has no media files in it.
+
+	As of now, doesn't handle read-only files.
+
+	:param dir_path: parent directory path
+	"""
 	abs_path = os.path.abspath(dir_path)
 	for path, dirs, files in os.walk(abs_path):
 		for directory in dirs:
@@ -157,13 +203,19 @@ def clear_remains(dir_path: str) -> None:
 					# print("trying to delete {}\\{}".format(path, directory))
 					rmtree(os.path.join(path, directory))
 				except Exception as error:
-					print("[ERROR] Could not delete directory: {}".format(directory))
+					print("[ERROR] Could not delete directory: '{}'".format(directory))
 					print("\t{}".format(error))
 				else:
-					print("[!] Directory {} deleted successfully.".format(directory))
+					print("[!] Directory '{}' deleted successfully.".format(directory))
 
 
 def contains_no_media(dir_path: str) -> bool:
+	"""
+	Checks if directory contains media files.
+
+	:param dir_path: directory path
+	:return: False if contains media files, True otherwise
+	"""
 	for item in os.listdir(dir_path):
 		item_path = os.path.join(dir_path, item)
 		if (os.path.isfile(item_path) and is_media_file(item_path)) \
@@ -172,16 +224,49 @@ def contains_no_media(dir_path: str) -> bool:
 	return True
 
 
+def fetch_album_art(dir_path: str) -> None:
+	"""
+	Downloads album art for all albums in given directory, and all its sub-directories.
+
+	:param dir_path: directory path
+	"""
+	tag = None
+	network = pylast.LastFMNetwork(api_key='d43c497febfef4ba166a51eca0932b90',  # TODO try catch?
+								   api_secret='1ce6e93f6e2c1329262484f41901ad2c')  # source code doesn't seem to require
+	for path, dirs, files in os.walk(dir_path):
+		if not contains_no_media(path) and (files[0] if files else None) \
+				and "cover_art.jpg" not in files:
+			tag = generate_tag(os.path.join(path, files[0]))
+			if tag:
+				try:
+					album = network.get_album(tag.artist, tag.album)
+					image_url = album.get_cover_image()  # by default returns 300x300 sized image
+					image_url = image_url.replace('300x300', '600x600')
+					request.urlretrieve(image_url, os.path.join(path, "cover_art.jpg"))
+				except pylast.MalformedResponseError or pylast.NetworkError as error:
+					print("[ERROR] Could not download album art for:\t'{}'".format(tag))
+					print("\t{}".format(error))
+				except Exception as error:
+					print("[ERROR] Unknown error occurred, please investigate.")
+					print("\t{}".format(tag))
+					print("\t{}".format(error))
+				else:
+					print("[!] Album art successfully retrieved for {}".format(tag))
+
+
 def main():
 	root = "D:\\CodeProjects\\Python\\music_test_folder"
 
-	log_file = open(os.path.join(root, 'organizer.log'), 'w')
-
 	organize(root)
 	clear_remains(root)
-
-	log_file.close()
+	fetch_album_art(root)
 
 
 if __name__ == '__main__':
 	main()
+
+# ========= Last.fm API account ========= #
+# Application name	music_organizer
+# API key	d43c497febfef4ba166a51eca0932b90
+# Shared secret	1ce6e93f6e2c1329262484f41901ad2c
+# Registered to	nivgmann
